b7d670c801f7fc4242bef346c7a041bc70307f03
LiliRedShoes <evaporite@hotmail.com>
2019-02-12 11:54:03 +0100

    premire étape de l'inscription à un sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -9,7 +9,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Sortie;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class SortieController extends AbstractController
 {
@@ -31,4 +34,47 @@ class SortieController extends AbstractController
 
     public function DeleteAction($id)
     {
-    }}
+    }
+
+    public function InscrireSortie (int $idParticipant, int $idSortie) {
+        $sortie= new Sortie();
+        $participant = new User();
+        //associer les objects sortie et user entre eux
+
+
+        //TODO gérer l'accès en fonction des roles
+        //TODO gérer la récupéréation de l'ID participant et id sortie
+
+        // récupérer l'objet sortie en entier
+        $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+        $sortie=$sortieRepository->findOneBy(['id'=>$idSortie]);
+
+        //récupérer l'objet participant en entier
+        $participantRepository = $this->getDoctrine()->getRepository(User::class);
+        $participant=$participantRepository->findOneBy(['id'=>$idParticipant]);
+
+        $sortie->addUser($participant);
+        $participant->addSortiesInscrit($sortie);
+
+        // récupére l'objet entity manager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($sortie);
+        $em->persist($participant);
+
+        //ajouter en base
+        $em->flush();
+
+        return $this->render('famille/monCompteFamille.html.twig');
+    }
+
+    public function desisterSortie () {
+// récupérer la sortie et IDparticipants
+        $sortieRepository = $this->getDoctrine()->getRepository(Sortie::class);
+        $sortieParticipant=$sortieRepository->findBy(array('idParticipant'=>$idParticipant,'idSortie'=>$idSortie));
+        //recupere entity manager
+        $em = $this->getDoctrine()->getManager();
+        //supprimer une instance
+        $em->remove($sortieParticipant);
+        $em->flush();
+    }
+}
