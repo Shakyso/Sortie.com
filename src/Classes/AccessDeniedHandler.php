cfa926bb71bc95efa0411cc9d94edf249a494d02
hduss <maintenancedotnet3@gmail.com>
2019-02-18 15:03:22 +0100

    debut css profil public

--- src/Classes/AccessDeniedHandler.php
+++ src/Classes/AccessDeniedHandler.php
@@ -6,12 +6,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
-public function handle(Request $request, AccessDeniedException $accessDeniedException)
-{
-    $content = '<p>je suis une page d\'erreur</p>';
-    return new Response($content, 403);
+
+
+    /**
+     * Page d'erreur pour les problèmes d'accès
+     * @param Request $request
+     * @param AccessDeniedException $accessDeniedException
+     * @return Response
+     */
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content = '<p>je suis une page d\'erreur</p>';
+        return new Response($content, 403);
+
+        //return $this->router->render()
     }
 }
